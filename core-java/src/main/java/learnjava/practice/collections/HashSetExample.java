c2aa9cd0a7f6238c55c633f5bd995a10cb57fd65
vishnu ponnam <your_email@domainname.com>
2019-04-10 16:31:08 -0400

    set

---
+++ core-java/src/main/java/learnjava/practice/collections/HashSetExample.java
@@ -0,0 +1,12 @@
+package learnjava.practice.collections;
+/*
+ *No Duplicates allowed
+ *No order Guaranteed
+ *Backed up by HashMap
+ *
+ */
+public class HashSetExample {
+public static void main(String[] args) {
+	
+}
+}
---
+++ core-java/src/main/java/learnjava/practice/collections/SetExmaple.java
@@ -0,0 +1,13 @@
+package learnjava.practice.collections;
+
+/*
+ *No Duplicates allowed
+ *No order Guaranteed
+ *
+ */
+public class SetExmaple {
+	
+	public static void main(String[] args) {
+		
+	}
+}
