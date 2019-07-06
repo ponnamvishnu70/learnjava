f5089a8ef0b239a77ad79d2052ba5573da9c0e2f
vishnu ponnam <your_email@domainname.com>
2019-07-06 16:41:10 -0400

    spring core

--- spring-core/src/main/java/learnjava/practice/springcore/javabasedconfiguration/JavaBasedConfigExample.java
+++ spring-core/src/main/java/learnjava/practice/springcore/javabasedconfiguration/JavaBasedConfigExample.java
@@ -16,7 +16,8 @@ public class JavaBasedConfigExample {
 
 	@Bean(initMethod = "init", destroyMethod = "cleanup" )//only for this bean life cycle call back will be called. 
 	//if you write @postconstruct and predestroy then the call back method will be call for all the Person Beans
-	@Primary//If there are multiple beans of same type .you can make one of as primary so that spring container know what is default bean
+	//If there are multiple beans of same type .you can make one of as primary so that spring container know what is default bean
+	@Primary
 	public Person person1() {
 		return new Person("vishnuuuu",28);
 	}
@@ -26,7 +27,7 @@ public class JavaBasedConfigExample {
 		return new Person("ponnammmmm",30);
 	}
 	
-	//the bean id is name of the method
+	@Bean//the bean id is name of the method
 	@Scope("prototype")//default is singleton
 	public Person person3() {
 		return new Person("ponnammmmm",30);
--- spring-core/src/main/java/learnjava/practice/springcore/javabasedconfiguration/JavaBasedConfigExample2.java
+++ spring-core/src/main/java/learnjava/practice/springcore/javabasedconfiguration/JavaBasedConfigExample2.java
@@ -8,6 +8,7 @@ public class JavaBasedConfigExample2 {
 
 	@Bean(name="person3")//This id  qualifier
 	public Person person2() {
-		return new Person("vishnu ponnam",29);
+		return new Person("vishnu ponnam70",29);
 	}
+	
 }
